9ece7f6a77232d33eab3a2b259b904e6d274d357
Robbie Clarken <rclarken@gmail.com>
2012-06-26 20:19:01 +1000

    Use environment variable to find libca

---
+++ test/test.js
@@ -0,0 +1,32 @@
+var epics = require('../main');
+
+//var pvName = 'SR11BCM01:CURRENT_MONITOR';
+//var pvName = 'CR01OPI06:BOX_UP_TIME_MONITOR';
+var pvName = 'CR01:GENERAL_ANALOG_05_MONITOR';
+var pv = new ca.Channel(pvName);
+
+pv.on('connection',function(data){
+  console.log('connection:',data);
+});
+
+pv.on('value',function(data) {
+  console.log('value:',data);
+});
+
+pv.connect(function(err){
+  if(err) {
+    console.log(err);
+    return;
+  }
+  pv.monitor();
+  /*
+  pv.getPV({},function(err,data) {
+    console.log('Get:',data);
+  });
+ */
+});
+
+var loop = function() {
+  setTimeout(loop,5000);
+};
+loop();
